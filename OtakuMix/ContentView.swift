291a2c5ea3739c98dc9fd9fe3804f6ca132a60da
luanolimpio <luanmaia05@hotmail.com>
2025-07-16 12:09:21 -0300

    Initial Commit

---
+++ OtakuMix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OtakuMix
+//
+//  Created by Luan Olimpio on 16/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OtakuMix/OtakuMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtakuMixApp.swift
+//  OtakuMix
+//
+//  Created by Luan Olimpio on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct OtakuMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
